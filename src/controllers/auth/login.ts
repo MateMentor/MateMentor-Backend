8171906217ccb6a7abb0a607ff80d1fd52d21953
Diego Prieto <prieto.diego@proton.me>
2024-01-03 11:30:14 +0100

    Add login functionality: Implement controller, install JWT, create route, and integrate with server. Tested and functional with Postman.

--- src/controllers/auth/login.ts
+++ src/controllers/auth/login.ts
@@ -1,1 +1,44 @@
+/** @format */
 
+// Import necessary modules and models
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../../models/User";
+
+async function login(req: Request, res: Response): Promise<Response | void> {
+        try {
+                const { email, password } = req.body;
+
+                const user = await User.findOne({ where: { email } });
+                if (!user) {
+                        return res.status(404).send("User not found");
+                }
+
+                const isValid = await bcrypt.compare(password, user.password);
+                if (!isValid) {
+                        return res.status(401).send("Invalid password");
+                }
+
+                const token = jwt.sign(
+                        { id: user.id },
+                        process.env.JWT_SECRET!,
+                        { expiresIn: "8h" },
+                );
+
+                return res.status(200).send({
+                        message: "Logged in successfully",
+                        token,
+                        user: {
+                                id: user.id,
+                                username: user.username,
+                                email: user.email,
+                        },
+                });
+        } catch (err) {
+                console.error(err);
+                return res.status(500).send("An unexpected error occurred");
+        }
+}
+
+export default login;
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,9 +2,11 @@
 
 import express from "express";
 import register from "../controllers/auth/register";
+import login from "../controllers/auth/login";
 
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/login", login);
 
 export default router;
